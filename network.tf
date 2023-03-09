e6adb6bef42a2f2778af24124ff4c6b69035da5b
Saverio Proto <saverioproto@microsoft.com>
2023-03-09 11:11:43 +0100

    Point vnet module to official Azure/subnets/azurerm

--- network.tf
+++ network.tf
@@ -1,5 +1,6 @@
 module "vnet_one" {
-  source              = "git::https://github.com/lonegunmanb/terraform-azurerm-subnets.git"
+  source              = "Azure/subnets/azurerm"
+  version             = "1.0.0"
   resource_group_name = azurerm_resource_group.this.name
   subnets = {
     subnet0 = {
@@ -12,7 +13,8 @@ module "vnet_one" {
 }
 
 module "vnet_two" {
-  source              = "git::https://github.com/lonegunmanb/terraform-azurerm-subnets.git"
+  source              = "Azure/subnets/azurerm"
+  version             = "1.0.0"
   resource_group_name = azurerm_resource_group.rg_2.name
   subnets = {
     subnet0 = {
